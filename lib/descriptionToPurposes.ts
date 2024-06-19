7335827a290ade89c857290b9a93003e25ec7450
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-06-19 11:23:17 +0100

    feat: add generated purposes

--- lib/descriptionToPurposes.ts
+++ lib/descriptionToPurposes.ts
@@ -27,26 +27,31 @@ function cost(msg: AIMessageChunk) {
     return (completionTokens * 1.5 + promptTokens * .5) / 10 ** 6;
 }
 
-const engine = new QueryEngine();
+
+async function pageFetch() {
+    const engine = new QueryEngine();
+
+    let i = 0;
+    const record: Record<number, string> = {};
+    const bindings = await engine.queryBindings('SELECT ?concept ?definition ?label ?note WHERE { ?concept a <https://w3id.org/dpv#Purpose>; <http://www.w3.org/2004/02/skos/core#definition> ?definition; <http://www.w3.org/2004/02/skos/core#prefLabel> ?label. OPTIONAL { ?concept <http://www.w3.org/2004/02/skos/core#scopeNote> ?note }  }', { sources: ['https://w3id.org/dpv#'] });
+    const page = (await bindings.map(elem => (record[++i] = elem.get('concept')!.value, i) + '\t' + elem.get('definition')!.value + '\t' + elem.get('label')!.value + '\t' + elem.get('note')?.value).toArray()).join('\n');
+    return { record, page };
+}
+
+const prom = pageFetch();
 
 async function descriptionToPurposes(description: string = "Download certain Google Tools and save certain preferences, for example the number of search results per page or activation of the SafeSearch Filter. Adjusts the ads that appear in Google Search.") {
     console.log('descriptionToPurposes');
-    let i = 0;
-    const record: Record<number, string> = {};
-    const bindings = await engine.queryBindings('SELECT ?concept ?definition ?label ?note WHERE { ?concept a <https://w3id.org/dpv#Purpose>; <http://www.w3.org/2004/02/skos/core#definition> ?definition; <http://www.w3.org/2004/02/skos/core#prefLabel> ?label. OPTIONAL { ?concept <http://www.w3.org/2004/02/skos/core#scopeNode> ?note }  }', { sources: ['https://w3id.org/dpv#'] });
-    const page = (await bindings.map(elem => (record[i++] = elem.get('concept')!.value) + '\t' + elem.get('definition')!.value + '\t' + elem.get('label')!.value + '\t' + elem.get('note')?.value).toArray()).join('\n');
-    // const options = Object.values(record);
+    const { record, page }: { record: Record<number, string>; page: string; } = await prom;
 
-    console.log(page);
-    process.exit();
-    
-    
-    // const string = await stringify((await engine.resultToString(bindings, 'text/tab-separated-values')).data);
-
-    // const bindingsStream = await engine.queryBindings('SELECT ?concept WHERE { ?concept a <https://w3id.org/dpv#Purpose>  }', { sources: ['https://w3id.org/dpv#'] });
-    // const options = await bindingsStream.map(elem => elem.get('concept')!.value).toArray();
-
-    const parser = StructuredOutputParser.fromZodSchema(z.array(z.string().url()));
+    const parser = StructuredOutputParser.fromZodSchema(
+        z.object({
+            // results: z.array(z.string().url().refine(url => options.includes(url), `Value must be one of ${JSON.stringify(options)}`)),
+            explanation: z.string().describe("A precise explanation of the reasoning behind the answer. DO NOT quote the prompt in this field."),
+            results: z.array(z.number().refine(num => Object.keys(record).includes(num.toString()), `Value must be one of ${JSON.stringify(Object.keys(record))}`)).describe("An array of numbers representing the line indices of the terms that apply to the description."),
+            unmatchedConcepts: z.array(z.string()).optional().describe("An array of concepts in the description that were not available."),
+        })
+    );
 
     const chain = RunnableSequence.from([
         PromptTemplate.fromTemplate(
@@ -55,14 +60,9 @@ async function descriptionToPurposes(description: string = "Download certain Goo
         new ChatOpenAI({
             model: 'gpt-4o',
             cache: await LocalFileCache.create('./cache'),
+            // verbose: true,
         }),
-        StructuredOutputParser.fromZodSchema(
-            z.object({
-                // results: z.array(z.string().url().refine(url => options.includes(url), `Value must be one of ${JSON.stringify(options)}`)),
-                results: z.array(z.number().refine(num => Object.keys(record).includes(num.toString()), `Value must be one of ${JSON.stringify(Object.keys(record))}`)),
-                explanation: z.string(),
-            })
-        ),
+        parser,
       ]);
 
     const fallbackChain = new RunnableWithFallbacks({
@@ -73,50 +73,60 @@ async function descriptionToPurposes(description: string = "Download certain Goo
         ]
     });
 
-    const noNamePrompt = "\n Make sure to use the *description* of the term, which is on the same line, in assessing its relevance and *not* the name of the term.";
+    const noNamePrompt = true ? '' : "\n Make sure to use the *description* of the term, which is on the same line, in assessing its relevance and *not* the name of the term.";
       
     const response = await fallbackChain.invoke({
-        question: `Which of the terms in the below document apply to the description \"${description}\"${noNamePrompt}\n---\n${page}\n---\n.`,
+        question: `Following the given structure, please give a list of integers corresponding to the lines of the page that match the following description, and explain the reasoning behind the answer. Please select ALL rows that apply. If the description contains concepts that do not match the listed purposes then describe such concepts in as granular manner as possible in the unmatchedConcepts field.\"${description}\"${noNamePrompt}\n---\n${page}\n---\n.`,
         format_instructions: parser.getFormatInstructions(),
     });
 
     const { store } = await dereferenceToStore(path.join(__dirname, '..', 'purposes.n3'), { localFiles: true });
 
+    console.log(response)
     const results = response.results.map((num: number) => record[num]);
 
     // Remove any super classes
     const minimal = results.filter(elem => !results.some(other => other !== elem && store.has(quad(namedNode(elem), namedNode('http://www.w3.org/2004/02/skos/core#broaderTransitive'), namedNode(other)))));
 
-    return minimal;
+    return { minimal, response };
     // return [];
 }
 
 let i = 0;
 
 async function main() {
-    const descriptions: Record<string, OCD & { Purposes?: string[] }> = await ocd();
+    const descriptions: Record<string, OCD & { Purposes?: string[]; response?: Awaited<ReturnType<typeof descriptionToPurposes>>['response'] }> = await ocd();
+    let promises: Promise<void>[] = [];
+    
+    let i = 0
     for (const key in descriptions) {
-        i += 1;
-        if (i > 5) {
-            process.exit();
-        }
-        const description = descriptions[key].Description;
-        if (description) {
-            try {
-                console.log('trying...');
-                descriptions[key].Purposes = await descriptionToPurposes(description);
-                console.log(key, description, descriptions[key].Purposes);
-            } catch (e) {
-                console.warn("Failure: " + (i++));
-            }
+        promises.push(addResponse(descriptions, key));
+        if (i++ % 20 === 0) {
+            await Promise.all(promises);
         }
     }
-    fs.writeFileSync('./purposes.json', JSON.stringify(descriptions, null, 2));
+    fs.writeFileSync('./ocd.json', JSON.stringify(descriptions, null, 2));
+    // fs.writeFileSync('./purposes.json', JSON.stringify(descriptions, null, 2));
     console.log(descriptions);
 }
 
 main();
 
+
+async function addResponse(descriptions: Record<string, OCD & { Purposes?: string[] | undefined; response?: { explanation: string; results: number[]; unmatchedConcepts?: string[] | undefined; } | undefined; }>, key: string) {
+    const description = descriptions[key].Description;
+    if (description) {
+        try {
+            console.log('trying...');
+            const { minimal, response } = await descriptionToPurposes(description);
+            descriptions[key].Purposes = minimal;
+            descriptions[key].response = response;
+            console.log(key, description, descriptions[key].Purposes);
+        } catch (e) {
+            console.warn("Failure: " + (i++) + e);
+        }
+    }
+}
 // Gets the list of DPV purposes that apply to a description.
 // FUTURE: Invent terms that make use of the DPV ontology.
 // descriptionToPurposes().then(console.log);
